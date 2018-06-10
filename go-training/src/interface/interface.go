e1a55b8ad809e0ce825d6171c6acde703d822504
LyonRu <lyonru29@163.com>
2018-06-11 07:09:52 +0800

    add interface example

--- go-training/src/interface/interface.go
+++ go-training/src/interface/interface.go
@@ -15,15 +15,45 @@ type Teacher struct {
 func (teacher Teacher) Print() {
 	fmt.Println(teacher.Name)
 }
+func (teacher Teacher) teach() {
+	fmt.Println("teacher teaching...")
+}
 
+// so Principal owns A Teacher
+// that it will has an Eat method
+// that it is a Ihuman too
+type Principal struct {
+	Teacher
+	Title string
+}
+
+func (pricipal Principal) Power() {
+	fmt.Println("pricipal has highest power in the school")
+}
+
+func (pricipal Principal) teach() {
+	pricipal.Teacher.teach()
+	fmt.Println("pricipal teaching...")
+}
+
+//implements of Eat method
+//so teacher is a Ihuman
 func (teacher Teacher) Eat() {
 	fmt.Println("eating food")
 }
+
 func main() {
 
 	var humam Ihuman = Teacher{"tom"}
+
 	humam.Eat()
+
 	if humam, ok := humam.(Ihuman); ok {
 		humam.Eat()
 	}
+
+	humam2 := &Principal{Teacher{}, "principal"}
+	//actually call teacher.teach(),unless
+	// unless principal has teach implements
+	humam2.teach()
 }
